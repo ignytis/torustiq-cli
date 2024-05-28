7459ce0aee18ecfaf35b3f27369b51e8d4067a33
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-05-28 23:59:24 +0300

    Dynamic module loading

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{fs, io};
-use std::error::Error;
+use std::{env, error::Error, ffi::CStr, fs, io, str};
 
 use clap::{arg, command, Parser};
-
+use log::debug;
 use serde::{Serialize, Deserialize};
+
+use torustiq_common::ffi::module::Module;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct PipelineStep {
@@ -11,12 +12,10 @@ struct PipelineStep {
     handler: String,
 }
 
-
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct PipelineDefinition {
     steps: Vec<PipelineStep>,
 }
-
 
 /// Starts a data processing pipeline from provided config
 #[derive(Parser, Debug)]
@@ -25,18 +24,47 @@ struct Args {
     /// A YAML file to read the pipeline structure from
     #[arg(short, long, default_value="pipeline.yaml")]
     pipeline_file: String,
+
+    /// A YAML file to read the pipeline structure from
+    #[arg(short, long, default_value="modules")]
+    module_dir: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info")
+    }
+    env_logger::init();
+
     let args = Args::parse();
-    let cfg: String = match fs::read_to_string(&args.pipeline_file) {
+
+    let pipeline_cfg: String = match fs::read_to_string(&args.pipeline_file) {
         Ok(c) => c,
         Err(e) => return err(format!("Cannot open the pipeline file: '{}'. {}", args.pipeline_file, e)),
     };
-    let _cfg: PipelineDefinition = match serde_yaml::from_str(cfg.as_str()) {
+    let _pipeline_cfg: PipelineDefinition = match serde_yaml::from_str(pipeline_cfg.as_str()) {
         Ok(c) => c,
         Err(e) => return err(format!("Cannot parse the pipeline: '{}'. {}", args.pipeline_file, e)),
     };
+
+    for entry in fs::read_dir(args.module_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        unsafe {
+            let lib = libloading::Library::new(&path)?;
+            let func: libloading::Symbol<unsafe extern fn() -> Module> = lib.get(b"load")?;
+            let module = func();
+
+            let module_name: &CStr = CStr::from_ptr(module.name);
+            let module_name: &str = module_name.to_str().unwrap();
+            let module_ref_id: &CStr = CStr::from_ptr(module.ref_id);
+            let module_ref_id: &str = module_ref_id.to_str().unwrap();
+
+            debug!("Module loaded: {} (ref_id: {}, path: {})", module_name, module_ref_id, path.into_os_string().into_string().unwrap());
+        }
+    }
+
     Ok(())
 }
 
