de7aa3fe527cf510bc88cae0aa8b12c8db21701b
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-06-04 01:02:56 +0300

    Pipeline build WIP

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,10 +1,12 @@
-use std::str;
+use std::{collections::HashMap, str};
 
 use serde::{Serialize, Deserialize};
 
+use torustiq_common::types::module::Module;
+
 /// A step in pipeline: source, destination, transformation, etc
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct PipelineStep {
+pub struct PipelineStepDefinition {
     pub name: String,
     pub handler: String,
 }
@@ -12,5 +14,23 @@ pub struct PipelineStep {
 /// A pipeline definition. Contains multiple steps
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct PipelineDefinition {
-    pub steps: Vec<PipelineStep>,
+    pub steps: Vec<PipelineStepDefinition>,
+}
+
+pub struct Pipeline {
+    pub steps: Vec<Module>,
+}
+
+impl Pipeline {
+    fn new() -> Pipeline {
+        Pipeline {
+            steps: Vec::new(),
+        }
+    }
+
+    pub fn build(definition: &PipelineDefinition, modules: &HashMap<String, Module>) -> Pipeline {
+        let mut pipeline = Pipeline::new();
+
+        pipeline
+    }
 }
