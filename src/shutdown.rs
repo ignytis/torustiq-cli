ed061d869bd415ac8e383a7d03f2c6a2d603d32f
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-11-12 01:04:18 +0200

    Force shutdown on receiving signal twice

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -1,24 +1,38 @@
+use std::{process::exit, sync::atomic::{AtomicBool, Ordering}};
+
 use log::{error, info};
 
 use crate::xthread::PIPELINE;
+
+static IS_GRACEFUL_SHUTDOWN_REQUESTED: AtomicBool = AtomicBool::new(false);
 
 /// Initializes a system signal handler (e.g. handles CTRL+C)
 pub fn init_signal_handler() -> Result<(), String> {
     match ctrlc::set_handler(|| {
         info!("Received a termination signal in main thread");
+        match IS_GRACEFUL_SHUTDOWN_REQUESTED.load(Ordering::SeqCst) {
+            false => { // Signal received once: shutdown gracefully
+                info!("Shutting down gracefully...");
+                IS_GRACEFUL_SHUTDOWN_REQUESTED.store(true, Ordering::SeqCst);
 
-        let pipeline = unsafe {
-            match PIPELINE.get() {
-                Some(p) => p,
-                None => {
-                    error!("Cannot receive a pipeline singleton");
-                    return;
-                }
+                let pipeline = unsafe {
+                    match PIPELINE.get() {
+                        Some(p) => p,
+                        None => {
+                            error!("Cannot receive a pipeline singleton");
+                            return;
+                        }
+                    }
+                };
+
+                let pipeline = pipeline.lock().unwrap();
+                pipeline.trigger_termination();
+            },
+            true => { // force shutdown
+                info!("A graceful shutdown had been requested already. Shutting down forcefully...");
+                exit(-1)
             }
         };
-
-        let pipeline = pipeline.lock().unwrap();
-        pipeline.trigger_termination();
     }) {
         Ok(_) => Ok(()),
         Err(e) => Err(format!("Failed to init a signal handler: {}", e)),
