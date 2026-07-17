673963e5cd61faffb7f9d48dd32abcdfe0767fd4
Ignytis <155588001+ignytis@users.noreply.github.com>
2026-07-17 22:21:59 +0300

    Implemented directory reading

--- src/plugins/builtin/file/file_reader.cpp
+++ src/plugins/builtin/file/file_reader.cpp
@@ -3,6 +3,7 @@
 #include <spdlog/spdlog.h>
 #include <torustiq_sdk/message.h>
 
+#include <filesystem>
 #include <fstream>
 
 #include "typedefs.hpp"
@@ -26,25 +27,41 @@ void startFileLinesReader(StageInstance* instance) {
         msg.payload = reinterpret_cast<uint8_t*>(line.data());
         instance->hostGlobals.sendMessageFnPtr(stageHandle, &msg);
     }
-
-    TorustiqMessage msg = torustiq_message_create_eof();
-    instance->hostGlobals.sendMessageFnPtr(stageHandle, &msg);
-
-    spdlog::debug("file :: Reached EOF in reader with handle {}", stageHandle);
 }
 
 void startDirectoryFilesReader(StageInstance* instance) {
-    spdlog::error("Directory files mode is not yet implemented");
-    exit(EXIT_FAILURE);
+    TorustiqPluginStageHandle stageHandle = instance->stageHandle;
+    for (const filesystem::directory_entry& entry :
+         filesystem::directory_iterator(instance->path)) {
+        TorustiqMessage msg{};
+        ifstream file(entry.path());
+
+        std::ostringstream content;
+        content << file.rdbuf();
+        string contentStr = content.str();
+
+        msg.type = TORUSTIQ_MESSAGE_TYPE_DATA;
+        msg.payload_size = contentStr.size();
+        msg.payload = reinterpret_cast<uint8_t*>(contentStr.data());
+        instance->hostGlobals.sendMessageFnPtr(stageHandle, &msg);
+    }
 }
 
 }  // namespace
 
 void TorustiqCli::Plugins::Builtin::File::startReader(StageInstance* instance) {
+    TorustiqPluginStageHandle stageHandle = instance->stageHandle;
     switch (instance->contentsMode) {
         case StageInstanceContentsMode::FILE_LINES:
             startFileLinesReader(instance);
+            break;
         case StageInstanceContentsMode::DIRECTORY_FILES:
             startDirectoryFilesReader(instance);
+            break;
     }
+
+    TorustiqMessage msg = torustiq_message_create_eof();
+    instance->hostGlobals.sendMessageFnPtr(stageHandle, &msg);
+
+    spdlog::debug("file :: Reached EOF in reader with handle {}", stageHandle);
 }
